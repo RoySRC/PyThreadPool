6a6eb5e28a26581a6fb636024bccc5082e19d69f
RoySRC <roysajeeb4@gmail.com>
2020-07-30 11:22:46 -0600

    Updated example

--- example.py
+++ example.py
@@ -1,18 +1,22 @@
 from ThreadPool import ThreadPool
 import time
 
-# Function to be executed in a thread
-def function():
-    print("This is a function")
-    time.sleep(1)
-    print("Function done.")
-    
+# Function to be executed by the thread pool
+def sum(*args):
+	sum = 0
+	thread = -1
+	for i,arg in enumerate(args):
+		sum += arg
+		if i+1==len(args):
+			thread = i
+	print("Thread", thread, "sum:", sum)
+
 # Instantiate a thread pool with 50 worker threads
 pool = ThreadPool(50)
 
 # Add the functions to be executed by the threadpool
 for _ in range(5):
-    pool.add_task(function)
+    pool.add_task(sum, 1, 2, 3, _)
 
 # This is to prevent the threads from still running in the background
 pool.kill_all_threads()
