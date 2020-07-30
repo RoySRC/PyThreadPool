0ca03d7e1f9da417e4bbe8e9f1d583f71b207f02
RoySRC <roysajeeb4@gmail.com>
2020-07-30 06:14:01 -0600

    first commit

---
+++ ThreadPool.py
@@ -0,0 +1,115 @@
+# base code adapted from:
+#    https://www.metachris.com/2016/04/python-threadpool
+
+import numpy as np
+from queue import Queue
+from threading import Thread
+
+class ThreadPool:
+    """
+    Thread pool containing a pool of worker threads that execute
+    tasks from the task queue.
+    """
+    def __init__(self, num_threads):
+        self.task_queue = Queue()
+        self.workers = []
+        self.worker_status = [False]*num_threads # store the status of the workers
+        for worker_name in range(num_threads):
+            self.workers.append(__Worker__(self.task_queue, worker_name, self.worker_status))
+
+    def add_task(self, func, *args, **kargs):
+        """
+        Add a task to the queue. Tasks are functions that are e
+        xecuted by the worker threads. Arguments to the function
+        can be passed in as additional arguments to add_task
+        function including keyword arguments.
+        """
+        self.task_queue.put((func, args, kargs))
+
+    def wait_completion(self):
+        """
+        Wait for the completion of all the tasks in the queue.
+        This is a blocking call that will block the calling thread
+        till all the tasks in the queue is done.
+        """
+        self.task_queue.join()
+    
+    def __kill__(self):
+        """ 
+        Kill the worker thread
+        """
+        raise __KillWorkerException__("Kill")
+    
+    def kill_all_threads(self, wait=True):
+        """ Kill all the threads in the pool """
+        # wait for the completion of the current tasks in the pool
+        if wait:
+            self.wait_completion()
+        # while there exists at least one worker that is alive
+        while np.any(self.worker_status):
+            # get all the alive workers
+            alive_workers = np.argwhere(self.worker_status).flatten()
+            # send a kill signal to all the alive workers
+            for alive_worker in alive_workers:
+                worker = self.workers[alive_worker]
+                self.add_task(self.__kill__)
+
+                
+class __KillWorkerException__(Exception):
+    """
+    Exception to kill worker thread
+    """
+    pass
+
+
+class __Worker__(Thread):
+    """ 
+    Thread executing tasks from a given tasks queue
+    """
+    def __init__(self, task_queue, name, status):
+        Thread.__init__(self)
+        self.task_queue = task_queue
+        self.index = int(name)
+        # indicate to the thread pool that this worker is alive
+        self.status = status
+        self.status[self.index] = True  
+        self.start()
+        
+    def run(self):
+        _run_ = True
+        while _run_:
+            # The following statement is a blocking call, i.e.
+            # it will block the thread if the task queue is empty
+            func, args, kargs = self.task_queue.get()
+            try:
+                func(*args, **kargs)
+                
+            except Exception as e:
+                
+                if isinstance(e, __KillWorkerException__):
+                    # indicate to the thread pool that this worker is dead
+                    self.status[self.index] = False
+                    _run_ = False
+                else:
+                    print(e)
+                
+            finally:
+                self.task_queue.task_done()
+                
+                
+## Example usage
+if __name__ == "__main__":
+
+    # Function to be executed in a thread
+    def wait_delay():
+        print("This is a function")
+
+    # Instantiate a thread pool with 50 worker threads
+    pool = ThreadPool(50)
+
+    # Add the functions to be executed by the threadpool
+    for _ in range(5):
+        pool.add_task(wait_delay)
+    
+    # This is to prevent the threads from still running in the background
+    pool.kill_all_threads()
